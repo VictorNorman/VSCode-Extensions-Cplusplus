9371495c914ae01cda3062155d650b59ad60556e
Victor Norman <vtn2@calvin.edu>
2022-03-27 16:51:11 -0400

    newClass and newHeader working

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	activateNewClass(context);
 	activateNewHeaderFile(context);
+	activateCreateImplStub(context);
 	console.log('extension helloworld activated');
 }
 
@@ -116,6 +117,88 @@ public:
 	vscode.workspace.applyEdit(wsedit);
 }
 
+/**
+ * If the user has the cursor on a prototype in the class definition in the .h file,
+ * open the .cpp file and create an implementation stub at the bottom of the .cpp file.
+ */
+const activateCreateImplStub = (context: vscode.ExtensionContext) => {
+	let disposable = vscode.commands.registerCommand('vtn2-c++-stuff.createImplStub', async () => {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor) {
+
+			/**
+			 * get the class and check if the cursor is in that class.
+			 * get the class name
+			 * identify the symbol that is selected -- the name of the method.
+			 * find the .cpp file.
+			 * at end of file, put
+			 *   everything before the selectionRange start
+			 *   class name + ::
+			 *   everything after the selectionRange start
+			 *   open and close curlies.
+			 */
+
+			const symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', activeEditor.document.uri);
+			if (! symbols) {
+				return;
+			}
+			const selection = activeEditor.selection;
+			const text = activeEditor.document.getText(selection);
+			// console.log('selection = ', text);
+
+			const classes = symbols.filter((symbol: vscode.DocumentSymbol) => symbol.kind === vscode.SymbolKind.Class);
+			// console.log('classes = ', classes);
+			// Find enclosing class of the selection.
+			const enclosingClassSymbol = classes.find((classSym: vscode.DocumentSymbol) =>
+				classSym.range.contains(selection)
+			);
+			if (!enclosingClassSymbol) {
+				return;
+			}
+
+			const symbolsInClasses = findChildSymbols(classes);
+			const methods = symbolsInClasses.filter((symbol: vscode.DocumentSymbol) => symbol.kind === vscode.SymbolKind.Method);
+			const methodSymbol = methods.find((methodSym: vscode.DocumentSymbol) =>
+				methodSym.range.contains(selection)
+			);
+			if (!methodSymbol) {
+				return;
+			}
+
+			const methodRangeText = activeEditor.document.getText(methodSymbol.range);
+
+			// For a prototype written like this:    SomeClass * getItem() const;
+			// methodSymbol.name is "getItem() const"
+			// methodRangeText is "SomeCLass * getItem() const;"
+			// so we need to isolate the return type in a string, which is everything in
+			//   methodRangeText up to where the method name starts.
+			//
+
+			const returnType = methodRangeText.substring(0, methodRangeText.indexOf(methodSymbol.name));
+
+			// NOTE: a space is left on the end of returnType for some reason, so we don't
+			// put any extra space in the output.
+			const implStr = `\n${returnType}${enclosingClassSymbol.name}::${methodSymbol.name} {\n}\n`;
+			// console.log(implStr);
+
+
+			const wsPath = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0].uri.fsPath;
+			const dotCppfilePath = vscode.Uri.file(wsPath + `/Junk.cpp`);
+			const cppFileEditor = await vscode.window.showTextDocument(dotCppfilePath);
+			cppFileEditor.edit((editBuilder: vscode.TextEditorEdit) => {
+				editBuilder.insert(new vscode.Position(100000, 0), implStr);
+			});
+
+		}
+	});
+	context.subscriptions.push(disposable);
+};
+
+function findChildSymbols(symbols: vscode.DocumentSymbol[]): vscode.DocumentSymbol[] {
+	const vars = symbols.filter(symbol => true);
+	return vars.concat(symbols.map(symbol => findChildSymbols(symbol.children))
+		.reduce((a, b) => a.concat(b), []));
+}
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
